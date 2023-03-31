54c35f0aa7a809af51684aa3014c76cc794c06a6
mmunkyeong <jmk4379@naver.com>
2023-03-31 14:12:48 +0900

    join fail go back

--- src/main/java/com/ll/gramgram/base/rq/Rq.java
+++ src/main/java/com/ll/gramgram/base/rq/Rq.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
